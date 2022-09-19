6dad3ef04350b23017f118a1f8149ed2b2f8ff28
LariiG <laribrun215@hotmail.com>
2022-09-19 17:48:01 -0300

    projeto mapa

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GooglemapComponent } from './googlemap/googlemap.component';
+
+const routes: Routes = [
+  {path:'',component:GooglemapComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/googlemap/googlemap.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-googlemap',
+  templateUrl: './googlemap.component.html',
+  styleUrls: ['./googlemap.component.css']
+})
+export class GooglemapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  display : any;
+  center: google.maps.LatLngLiteral = {lat: -22.9035, lng: -43.2096};
+  zoom = 12;
+
+  moveMap(event: google.maps.MapMouseEvent) {
+    if(event.latLng!= null)
+    this.center = (event.latLng.toJSON());
+  }
+
+  move(event: google.maps.MapMouseEvent) {
+    if(event.latLng != null)
+    this.display = event.latLng.toJSON();
+  }
+}
